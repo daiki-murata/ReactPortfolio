624a601c932e7c360cc58516b84390c7985e8f00
daiki-murata <muratadaiki@muratadaikiteki-MacBook-Pro.local>
2021-07-15 02:00:13 +0900

    3回目の修正

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,22 +1,30 @@
 import React from 'react'
-// import {  FaGithub } from "react-icons/fa";
 import classes from './Contact.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {motion} from 'framer-motion'
 
-// const handleURL = (url) => {
-//     return (
-//         () => window.open(url, '_blank')
-//     )
-// }
+const handleURL = (url) => {
+    return (
+        () => window.open(url, '_blank')
+    )
+}
 
 // 本命
 const Contact = () => {
+    const iconStyle = { padding: 10, fontSize: 50 };
+
     return(
         <div className = {classes.Contact} id='contact'>
             <div className={classes.ContactIcons}>
                 <motion.div
                 whileHover={{scale:1.3}}>
-                    {/* <FaGithub color='white' size='30px' style={{padding: '1%'}} onClick={handleURL('https://github.com/daiki-murata?tab=repositories')} /> */}
+                     <FontAwesomeIcon 
+                     className={classes.GithubIcon}
+                     icon={faGithub} 
+                     style={iconStyle} 
+                     color='white' 
+                     onClick={handleURL('https://github.com/daiki-murata?tab=repositories')} /> 
                 </motion.div>
             </div>
         </div>
